67c20d3d56205dbc87a98ca878bdede04d2f97f7
david kaye <dfkaye@yahoo.com>
2013-07-22 13:00:04 -0700

    Update css-request.js

--- js/css-request.js
+++ js/css-request.js
@@ -41,8 +41,9 @@
       // MSIE
       sheet = style.styleSheet;
       
-      console.dir(sheet)
-      
+      console.dir(sheet.rules)
+      console.dir(sheet.imports)
+
       //cssRules = sheet.rules;
       //length = cssRules.length;
       
