fb329748e04d32aefe7f0ebfd560679a0bf58799
david kaye <dfkaye@yahoo.com>
2013-07-23 09:57:19 -0700

    Create css-request-imports.js

---
+++ js/css-request-imports.js
@@ -0,0 +1,11 @@
+/*
+ * IE 9 and down to get around the total stylesheet, imports and css rules limits 
+ * wisely imposed by Microsoft's developers.
+ */
+ 
+;(function () {
+
+  alert('testing: should be IE 9 or less');
+  
+  
+}());
